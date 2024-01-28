5bd1222e9e587782726fe63909f8b0e9e1822d00
Bohdan Kolomiiets <kb907406@gmail.com>
2024-01-28 16:22:02 +0200

    Implement BorrowingCreateSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,4 +1,6 @@
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from book.serializers import BookSerializer
 from borrowing.models import Borrowing
@@ -37,3 +39,26 @@ class BorrowingListSerializer(BorrowingSerializer):
 class BorrowingDetailSerializer(BorrowingSerializer):
     book = BookSerializer(many=False, read_only=True)
 
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "borrow_date", "expected_return_date")
+
+    def validate(self, attrs):
+        data = super(BorrowingCreateSerializer, self).validate(attrs=attrs)
+        Borrowing.validate_book_inventory(
+            attrs["book"],
+            ValidationError,
+        )
+        return data
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            borrowing = Borrowing.objects.create(**validated_data)
+
+            book = validated_data.get("book")
+
+            book.decrease_inventory_when_borrowed()
+
+            return borrowing

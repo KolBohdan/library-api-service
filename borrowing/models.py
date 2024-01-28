b683ac410ff5c08a91bdc8e97504eff7ebc09a72
Bohdan Kolomiiets <kb907406@gmail.com>
2024-01-28 16:20:41 +0200

    Validate book inventory is not 0

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import CheckConstraint, Q, F
 
@@ -21,12 +22,39 @@ class Borrowing(models.Model):
         constraints = [
             CheckConstraint(
                 check=(
-                        Q(borrow_date__lte=F("expected_return_date"))
-                        & Q(borrow_date__lte=F("actual_return_date"))
+                    Q(borrow_date__lte=F("expected_return_date"))
+                    & Q(borrow_date__lte=F("actual_return_date"))
                 ),
-                name="borrow_date_lte_return_date"
+                name="borrow_date_lte_return_date",
             ),
         ]
+
+    @staticmethod
+    def validate_book_inventory(book, error_to_raise):
+        if book.inventory <= 0:
+            raise error_to_raise(
+                {
+                    "book_inventory": f"{book.title} is currently out of stock"
+                }
+            )
+
+    def clean(self):
+        Borrowing.validate_book_inventory(
+            self.book,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self) -> str:
         return f"{self.book} borrowed by {self.user} at {str(self.borrow_date)}"
